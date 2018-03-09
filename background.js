2b9260b41b7fe04e05fcce8f5bb7636a7ba79ba3
Elliott Beaty <elliott@inventorylab.com>
2018-03-09 10:16:36 -0700

    initial commit

---
+++ background.js
@@ -0,0 +1,72 @@
+﻿chrome.browserAction.onClicked.addListener(function (tab) {
+    getOptions().then(function (options) {
+        if (typeof options === 'undefined' ||
+            options === null ||
+            typeof options.baseUrl === 'undefined' ||
+            options.baseUrl === null ||
+            options.baseUrl.length < 2) { 
+            promptToNavigateToOptions();
+        } else {
+            //Navigate to ticket
+            var ticket = window.prompt("Please enter the Stratify Error ID", "Example: ca5dca2e-49b3-438f-b3e0-d4e69da3012d");
+            navigateToTicket(ticket, options);
+        }
+    });; 
+});
+function getSanitizedBaseUrl(url) {
+
+    if (typeof url === 'undefined' || url === null) {
+        throw Error("Url is null or undefined!");
+    }
+    // check for http
+    if (url.indexOf('http') === -1) {
+        url = 'http://' + url;
+    }
+    // check for tailing slash
+    var lastCharacter = url.substr(url.length - 1);
+    if (lastCharacter !== '/') {
+        url = url + '/';
+    }
+    return url;
+}
+function promptToNavigateToOptions() {
+    if (confirm('You need to configure this plugin first. Would you like to do that now?')) {
+        chrome.tabs.create({
+            url: '/options/options.html'
+        });
+    } else {
+        return;
+    }
+}
+
+function navigateToTicket(guid, options) {
+    if (typeof guid !== 'undefined' && guid !== null) {
+        var url = getSanitizedBaseUrl(options.baseUrl) + "?log_q=where(" + guid +")&last=Last%2020%20Minutes";
+        if (options.openInNewTab == 'true') {
+            chrome.tabs.create({
+                url: url
+            });
+        } else {
+            chrome.tabs.update({
+                url: url
+            });
+        }
+
+    }
+}
+function getOptions() {
+    return new Promise(function (successCallback) {
+        chrome.storage.sync.get(function (settings) {
+            var options = new Options(settings.baseUrl, settings.ticketPrefix, settings.openInNewTab);
+            successCallback(options);
+        });
+    });
+   
+}
+function Options(baseUrl, ticketPrefix, openInNewTab) {
+    var self = this;
+    self.baseUrl = baseUrl;
+    self.ticketPrefix = ticketPrefix;
+    self.openInNewTab = openInNewTab;
+    return self;
+}
---
+++ options/options.js
@@ -0,0 +1,59 @@
+﻿var $txtBaseUrl, $txtTicketPrefix, $selectOpenNewTab, $btnSave;
+$(document).ready(function () {
+   
+
+    $txtBaseUrl = $("#txtBaseUrl");
+    $txtTicketPrefix = $("#txtTicketPrefix");
+    $selectOpenNewTab = $("#selectNewTab");
+    $btnSave = $("#btnSave");
+
+    restore_options();
+    bindEventListeners();
+    $(document).foundation();
+});
+function bindEventListeners() {
+    
+    // Save
+    $btnSave.on('click', function () {
+       
+        save_options();
+    });
+}
+
+// Saves options to chrome.storage
+function save_options() {
+    var baseUrl = $txtBaseUrl.val();
+    var ticketPrefix = $txtTicketPrefix.val();
+    var openInNewTab = $selectOpenNewTab.val();
+
+    chrome.storage.sync.set({
+        baseUrl: baseUrl,
+        ticketPrefix: ticketPrefix,
+        openInNewTab: openInNewTab
+    }, function () {
+        closeOptionsTab();
+        });
+}
+function closeOptionsTab() {
+    chrome.tabs.getSelected(null, function (tab) {
+        chrome.tabs.remove(tab.id);
+    });
+}
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+        baseUrl: 'https://logentries.com/app/191129da#/search/logs/',
+        ticketPrefix: '',
+        openInNewTab: 'true'
+    }, function (settings) {
+        console.log(settings)
+        $txtBaseUrl.val(settings.baseUrl);
+        $txtTicketPrefix.val(settings.ticketPrefix);
+        console.log(settings.openInNewTab);
+        if (settings.openInNewTab == 'false') {
+            $('#selectNewTab').val('false');
+        }
+    });
+}
